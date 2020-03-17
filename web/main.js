025d79c4a2d9bdb44d77d5d7c056d7e5e8278da3
a122774007 <a122774007@gmail.com>
2020-03-17 15:47:39 +0800

    center slide complete

--- web/main.js
+++ web/main.js
@@ -1,27 +1,38 @@
-$( function() {
+$(function () {
     var startX;
+    var left_airplane = $("#airplane").css("left");
     $("#btn").draggable({
         axis: "x",
         containment: ".roll_bar",
-        drag: function(e){
-            //console.log(("#train").css("left"));
-            console.log($("#train").css("left").substring(0, $("#train").css("left").length - 2));
-            $("#train").css("left", (e.offsetX - startX) * 5);
+        drag: function (e) {
+            $("#train").css("left", "-=" + ((e.offsetX - startX) * 10));
+            $("#airplane").css("left", "-=" + ((e.offsetX - startX) * 10));
+            $(".mid").css("left", "-=" + ((e.offsetX - startX) * 10));
         },
-        start: function(e){
+        start: function (e) {
             startX = e.offsetX;
         },
-        stop: function(){
-            if($(this).position().left - $(".roll_bar").position().left <= 5){
+        stop: function (e) {
+            if ($(this).position().left - $(".roll_bar").position().left <= 5) {
                 alert("left");
             }
-            else if(($(".roll_bar").position().left + $(".roll_bar").width()) - ($(this).position().left + $(this).width()) <= 10){
+            else if (($(".roll_bar").position().left + $(".roll_bar").width()) - ($(this).position().left + $(this).width()) <= 10) {
                 alert("right");
             }
-            else{
+            else {
                 $(this).animate({
+                    left: "0"
+                }, 500);
+                $("#train").animate({
+                    left: "-350px"
+                }, 500);
+                $("#airplane").animate({
+                    left: left_airplane
+                }, 500);
+                $(".mid").animate({
                     left: "0",
-                }, 500);    
+                    width: "100%"
+                }, 500);
             }
         }
     });
