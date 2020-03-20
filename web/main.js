bdd46c521470df3355b765a1cc507465b4375ba2
a122774007 <a122774007@gmail.com>
2020-03-20 21:14:15 +0800

    Revert "test for no absolute"
    This reverts commit 58305106acd1f8b662779a052a42e2f49c43b419.

--- web/main.js
+++ web/main.js
@@ -1,32 +1,30 @@
 $(function () {
-    var originLeft = $("#btn").css("left");
-    console.log(originLeft);
+    var startX;
     var left_airplane = $("#airplane").css("left");
     $("#btn").draggable({
         axis: "x",
         containment: ".roll_bar",
         drag: function (e) {
-            /*
             $("#train").css("left", "-=" + ((e.offsetX - startX) * 5));
             $("#airplane").css("left", "-=" + ((e.offsetX - startX) * 5));
             $(".mid").css("left", "-=" + ((e.offsetX - startX) * 5));
-            */
         },
         start: function (e) {
+            startX = e.offsetX;
             left_airplane = $("#airplane").css("left");
         },
         stop: function (e) {
+            console.log($(this).position().left - $(".roll_bar").position().left);
             if ($(this).position().left - $(".roll_bar").position().left <= 5) {
-                //alert("left");
+                alert("left");
             }
             else if (($(".roll_bar").position().left + $(".roll_bar").width()) - ($(this).position().left + $(this).width()) <= 10) {
-                //alert("right");
+                alert("right");
             }
             else {
                 $(this).animate({
-                    left: originLeft
+                    left: "0"
                 }, 500);
-                /*
                 $("#train").animate({
                     left: "-1250px"
                 }, 500);
@@ -37,7 +35,6 @@ $(function () {
                     left: "0",
                     width: "100%"
                 }, 500);
-                */
             }
         }
     });
